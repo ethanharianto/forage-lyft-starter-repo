aa3562f6f702a3ac4374e674386329f7fec00e76
ethanharianto <ethanhanshr919@gmail.com>
2023-06-23 00:27:23 -0400

    task 3

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -1,5 +1,6 @@
 from battery.battery import Battery
 
+
 class NubbinBattery(Battery):
     def __init__(self, current_date, last_service_date):
         self.current_date = current_date
@@ -7,7 +8,7 @@ class NubbinBattery(Battery):
 
     def needs_service(self):
 
-        if self.last_service_date.replace(year=self.last_service_date.year + 2) < self.current_date:
+        if self.last_service_date.replace(year=self.last_service_date.year + 4) < self.current_date:
             return True
         else:
             return False
