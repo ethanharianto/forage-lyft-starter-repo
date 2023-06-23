2d2bf2b8e428d85c71755ff7fea48ae146e40e9f
ethanharianto <ethanhanshr919@gmail.com>
2023-06-23 00:21:08 -0400

    task 3

---
+++ test/test_battery.py
@@ -0,0 +1,35 @@
+import unittest
+
+from datetime import date
+
+from battery.nubbin_battery import NubbinBattery
+
+from battery.spindler_battery import SpindlerBattery
+
+
+class TestNubbinBattery(unittest.TestCase):
+    def test_needs_service_true(self):
+        current_date = date.fromisoformat("2023-06-23")
+        last_service_date = date.fromisoformat("2023-06-22")
+        battery = NubbinBattery(current_date, last_service_date)
+        self.assertTrue(battery.needs_service())
+
+    def test_needs_service_false(self):
+        current_date = date.fromisoformat("2023-06-23")
+        last_service_date = date.fromisoformat("2023-05-23")
+        battery = NubbinBattery(current_date, last_service_date)
+        self.assertFalse(battery.needs_service())
+
+
+class TestSpindlerBattery(unittest.TestCase):
+    def test_needs_service_true(self):
+        current_date = date.fromisoformat("2020-05-15")
+        last_service_date = date.fromisoformat("2018-01-25")
+        battery = SpindlerBattery(current_date, last_service_date)
+        self.assertTrue(battery.needs_service())
+
+    def test_needs_service_false(self):
+        current_date = date.fromisoformat("2020-05-15")
+        last_service_date = date.fromisoformat("2019-01-10")
+        battery = SpindlerBattery(current_date, last_service_date)
+        self.assertFalse(battery.needs_service())
